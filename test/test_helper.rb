8c7aa3763f5bbc7afa6436e0797d9e71e8d6f0ab
Julik Tarkhanov <me@julik.nl>
2023-11-02 16:34:48 +0100

    Try to fix an activesupport issue
    as per https://github.com/rails/rails/issues/49495

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,5 +4,6 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "pecorino"
 
 require "minitest/autorun"
+require "active_support"
 require "active_support/test_case"
 require "active_record"
