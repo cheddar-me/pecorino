5f5b682fcf17d7de547c779d74662632813c40fe
Julik Tarkhanov <me@julik.nl>
2024-01-03 12:39:01 +0000

    Switch on DB adapter

--- lib/pecorino.rb
+++ lib/pecorino.rb
@@ -7,6 +7,7 @@ require_relative "pecorino/version"
 require_relative "pecorino/leaky_bucket"
 require_relative "pecorino/throttle"
 require_relative "pecorino/postgres"
+require_relative "pecorino/sqlite"
 require_relative "pecorino/railtie" if defined?(Rails::Railtie)
 
 module Pecorino
