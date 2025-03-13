67b488bb795e1da1b5e0b0647f93f02773f32e37
Julik Tarkhanov <me@julik.nl>
2025-03-13 11:33:04 +0000

    Do not use .minutes

--- test/block_test.rb
+++ test/block_test.rb
@@ -11,7 +11,7 @@ class BlockTest < ActiveSupport::TestCase
   test "sets a block" do
     k = Base64.strict_encode64(Random.bytes(4))
     assert_nil Pecorino::Block.blocked_until(key: k)
-    assert Pecorino::Block.set!(key: k, block_for: 30.minutes)
+    assert Pecorino::Block.set!(key: k, block_for: 30 * 60)
 
     blocked_until = Pecorino::Block.blocked_until(key: k)
     assert_in_delta Time.now + 30.minutes, blocked_until, 10
@@ -20,7 +20,7 @@ class BlockTest < ActiveSupport::TestCase
   test "does not return a block which has lapsed" do
     k = Base64.strict_encode64(Random.bytes(4))
     assert_nil Pecorino::Block.blocked_until(key: k)
-    Pecorino::Block.set!(key: k, block_for: -30.minutes)
+    Pecorino::Block.set!(key: k, block_for: -30 * 60)
     blocked_until = Pecorino::Block.blocked_until(key: k)
     assert_nil blocked_until
   end
