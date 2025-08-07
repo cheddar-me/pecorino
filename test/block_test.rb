37b43e6ae0cb0072544425e1880d0ca5e80b880c
allockicmoi <allockicmoi@gmail.com>
2025-08-07 10:25:51 +0100

    Update block_test.rb

--- test/block_test.rb
+++ test/block_test.rb
@@ -10,18 +10,18 @@ class BlockTest < ActiveSupport::TestCase
 
   test "sets a block" do
     k = Base64.strict_encode64(Random.bytes(4))
-    assert_nil Pecorino::Block.blocked_until(key: k, adapter: @adapter)
-    assert Pecorino::Block.set!(key: k, block_for: 30.minutes, adapter: @adapter)
+    assert_nil Pecorino::Block.blocked_until(key: k, adapter: memory_adapter)
+    assert Pecorino::Block.set!(key: k, block_for: 30.minutes, adapter: memory_adapter)
 
-    blocked_until = Pecorino::Block.blocked_until(key: k, adapter: @adapter)
+    blocked_until = Pecorino::Block.blocked_until(key: k, adapter: memory_adapter)
     assert_in_delta Time.now + 30.minutes, blocked_until, 10
   end
 
   test "does not return a block which has lapsed" do
     k = Base64.strict_encode64(Random.bytes(4))
-    assert_nil Pecorino::Block.blocked_until(key: k, adapter: @adapter)
-    Pecorino::Block.set!(key: k, block_for: -30.minutes, adapter: @adapter)
-    blocked_until = Pecorino::Block.blocked_until(key: k, adapter: @adapter)
+    assert_nil Pecorino::Block.blocked_until(key: k, adapter: memory_adapter)
+    Pecorino::Block.set!(key: k, block_for: -30.minutes, adapter: memory_adapter)
+    blocked_until = Pecorino::Block.blocked_until(key: k, adapter: memory_adapter)
     assert_nil blocked_until
   end
 end
