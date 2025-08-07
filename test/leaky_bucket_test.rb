1f95dcaedbeedf0d4a95227a917915e8b8b1b75a
allockicmoi <allockicmoi@gmail.com>
2025-08-07 10:06:34 +0100

    Update leaky_bucket_test.rb

--- test/leaky_bucket_test.rb
+++ test/leaky_bucket_test.rb
@@ -5,6 +5,11 @@ require "test_helper"
 class LeakyBucketTest < ActiveSupport::TestCase
   def memory_adapter
     @adapter ||= Pecorino::Adapters::MemoryAdapter.new
+
+    # Create the Pecorino tables
+    ActiveRecord::Schema.define(version: 1) do |via_definer|
+      Pecorino.create_tables(via_definer)
+    end
   end
 
   # This test is performed multiple times since time is involved, and there can be fluctuations
