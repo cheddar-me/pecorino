969b732a16b132648b4bbadc35e21ab1f19dfbe4
allockicmoi <allockicmoi@gmail.com>
2025-08-07 10:44:09 +0100

    Update block_test.rb

--- test/block_test.rb
+++ test/block_test.rb
@@ -4,24 +4,24 @@ require "test_helper"
 require "base64"
 
 class BlockTest < ActiveSupport::TestCase
-  def memory_adapter
+  def setup
     @adapter = Pecorino::Adapters::MemoryAdapter.new
   end
 
   test "sets a block" do
     k = Base64.strict_encode64(Random.bytes(4))
-    assert_nil Pecorino::Block.blocked_until(key: k, adapter: memory_adapter)
-    assert Pecorino::Block.set!(key: k, block_for: 30.minutes, adapter: memory_adapter)
+    assert_nil Pecorino::Block.blocked_until(key: k, adapter: @adapter)
+    assert Pecorino::Block.set!(key: k, block_for: 30.minutes, adapter: @adapter)
 
-    blocked_until = Pecorino::Block.blocked_until(key: k, adapter: memory_adapter)
+    blocked_until = Pecorino::Block.blocked_until(key: k, adapter: @adapter)
     assert_in_delta Time.now + 30.minutes, blocked_until, 10
   end
 
   test "does not return a block which has lapsed" do
     k = Base64.strict_encode64(Random.bytes(4))
-    assert_nil Pecorino::Block.blocked_until(key: k, adapter: memory_adapter)
-    Pecorino::Block.set!(key: k, block_for: -30.minutes, adapter: memory_adapter)
-    blocked_until = Pecorino::Block.blocked_until(key: k, adapter: memory_adapter)
+    assert_nil Pecorino::Block.blocked_until(key: k, adapter: @adapter)
+    Pecorino::Block.set!(key: k, block_for: -30.minutes, adapter: @adapter)
+    blocked_until = Pecorino::Block.blocked_until(key: k, adapter: @adapter)
     assert_nil blocked_until
   end
 end
